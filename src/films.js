c5b6cfa539cb75ed60595826d607ef5b90ef9352
meritxell98 <meritxell98@hotmail.com>
2025-03-04 11:41:43 +0100

    Exercici 1: Get the array of all directors

--- src/films.js
+++ src/films.js
@@ -1,42 +1,37 @@
-function showResult (valor){
-  const resultDiv = document.getElementById('directors')
-  resultDiv.innerHTML = valor;
-}
-
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
 
-  const nomsDirectors = array.map((value) => value.director);
-  showResult(nomsDirectors)
+  return array.map((value) => value.director);
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+
   
 }
 
-// Exercise 4:  Alphabetic order by title 
+// // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
+function orderByYear(array) {
+
 
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
+function moviesAverageByCategory(array, category) {
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(array) {
 
 }
 
